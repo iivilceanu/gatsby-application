9df084aa05f9507fbcef4a2d5db91209229fd217
Ilie Vilceanu <31694421+ivilceanu@users.noreply.github.com>
2022-01-20 19:27:52 +0200

    newupdate2

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome my blog</p>
+    <p>Enjoy reading!</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -21,6 +21,7 @@ const IndexPage = () => (
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/page1/">Go to page 1</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
